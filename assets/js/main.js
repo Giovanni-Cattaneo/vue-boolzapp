2b28173c68b7a4915e6914dc975d5898317997da
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-14 14:20:14 +0100

    add change of chat with click on contact list

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,12 +36,12 @@ createApp({
                         hour: "15:45",
                         messages: [{
                             date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso il gatto?',
+                            message: 'Ti sei ricordato di andare a ritirare quella raccomandata in posta?',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di stendere i i cattivi',
+                            message: 'E se riesci pass anche al supermercato che abbiamo finito lo zucchero a velo per la torta della nonna',
                             status: 'sent'
                         },
                         {
@@ -57,12 +57,12 @@ createApp({
                         hour: "15:45",
                         messages: [{
                             date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso la tartaruga?',
+                            message: 'Hai porato il cane dal veterinario per quella pucle??',
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di appianare le divergenze',
+                            message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
                             status: 'sent'
                         },
                         {
@@ -78,37 +78,49 @@ createApp({
                         hour: "15:45",
                         messages: [{
                             date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso il cavallo?',
+                            message: 'riesci ad andare a prendere Carlotta a d equitazione?',
                             status: 'sent'
                         },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di pulire la stalla',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 16:15:22',
-                            message: 'Tutto fatto!',
-                            status: 'received'
-                        }]
+
+                        ]
                     },
                     {
                         image: "./assets/img/avatar_4.jpg",
                         fullname: "Alessandro de Bortoli",
                         activity: "Non attivo da circa 3h",
-                        hour: "15:45"
+                        hour: "15:45",
+                        messages: [
+                            {
+                                date: '10/01/2020 15:50:00',
+                                message: 'Si trova in via tagliamento vicno al sentiero che costeggia il lambro',
+                                status: 'sent'
+                            },
+                        ]
                     },
                     {
                         image: "./assets/img/avatar_5.jpg",
                         fullname: "Giuliano Vincenti",
                         activity: "Non attivo da circa 3h",
-                        hour: "15:45"
+                        hour: "15:45",
+                        messages: [
+                            {
+                                date: '10/01/2020 16:15:22',
+                                message: 'Tutto fatto!',
+                                status: 'received'
+                            }
+                        ]
                     },
                     {
                         image: "./assets/img/avatar_6.jpg",
                         fullname: "Loretta Scott king",
                         activity: "Non attivo da circa 3h",
-                        hour: "15:45"
+                        hour: "15:45",
+                        messages: [{
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }]
+
                     },
                     {
                         image: "./assets/img/avatar_7.jpg",
@@ -118,7 +130,7 @@ createApp({
                     },
                 ],
             inputText: "",
-            activeContact: 0,
+            activeContact: null,
 
         }
 
@@ -126,10 +138,26 @@ createApp({
 
     methods: {
 
-        contactClick(i) {
+        contactClick(index) {
             console.log("Hello world");
-            this.activeContact = i
-            console.log(this.activeContact);
+
+            if (index >= 0 && index < this.contacts.length) {
+                this.activeContact = index
+
+                const user = document.querySelector(".user")
+
+                const answer = document.querySelector(".answer")
+
+                const avatar = document.getElementById("main_avatar")
+
+                const intestation = document.getElementById("chat-intestation") 
+                console.log(user);
+
+                user.innerHTML = `<p>${this.contacts[index].messages[0].message}</p>`
+                user.innerHTML += `<p>${this.contacts[index].messages[1].message}</p>`
+                answer.innerHTML = `<p>${this.contacts[index].messages[2].message}</p>`
+            }
+
         },
 
         contactSearch() {
@@ -138,16 +166,16 @@ createApp({
             let textSearch = this.inputText.toLowerCase().trim()// prende il testo dell'input, lo mette in minuscolo e rimuove eventuali spazi all'inizio e alla fine
 
             // if (textSearch === "") {
-                 //return, capire come riportare la lista di contatti alla situazione di partenza
+            //return, capire come riportare la lista di contatti alla situazione di partenza
             // }
-            this.contacts = this.contacts.filter( function (contact){
+            this.contacts = this.contacts.filter(function (contact) {
 
-                if(contact.fullname.toLowerCase().includes(textSearch)){ // filtra e restiuisce il fullname in minuscolo se corrisponde a quello dell'input
+                if (contact.fullname.toLowerCase().includes(textSearch)) { // filtra e restiuisce il fullname in minuscolo se corrisponde a quello dell'input
                     return contact.fullname
-                }else{
-                    return textSearch = ""
+                } else {
+                    return
                 }
-                 
+
             })
 
         }
