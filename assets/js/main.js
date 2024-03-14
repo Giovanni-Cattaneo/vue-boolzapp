2f272568d02f765f4ee688a21cc2c169d3e7635a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-14 10:16:30 +0100

    add dynamic contact list in main js and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,49 @@ createApp({
                 {
                     image: "./assets/img/avatar_1.jpg",
                     fullname: "Giovanni cattaneo",
-                    Activity: "Non attivo da circa 3h",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_1.jpg",
+                    fullname: "Fabio De Luigi",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_2.jpg",
+                    fullname: "Michele Lamperti",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_3.jpg",
+                    fullname: "Samuele Bonomi",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_4.jpg",
+                    fullname: "Alessandro de Bortoli",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_5.jpg",
+                    fullname: "Giuliano Vincenti",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_6.jpg",
+                    fullname: "Loretta Scott king",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+                {
+                    image: "./assets/img/avatar_7.jpg",
+                    fullname: "Luciano Cadorna",
+                    activity: "Non attivo da circa 3h",
                     hour: "15:45"
                 },
             ]
