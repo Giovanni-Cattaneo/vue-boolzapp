9347e427b15b22d1a2eba0f19d9adfafdbe74eb2
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-14 10:07:43 +0100

    edit error in contact container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 console.log("Hello world");
+
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return{
+            contacts : 
+            [
+                {
+                    image: "./assets/img/avatar_1.jpg",
+                    fullname: "Giovanni cattaneo",
+                    Activity: "Non attivo da circa 3h",
+                    hour: "15:45"
+                },
+            ]
+        }
+    },
+
+    methods(){
+
+    }
+}).mount("#app")
