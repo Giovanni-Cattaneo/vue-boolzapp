850054687a0aeb804b097c4b9399b5e16c065e1a
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-15 11:01:16 +0100

    add contactrestore, when delete name in input search contact now restore list of contacts

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,7 +190,191 @@ createApp({
                 ],
             inputText: "",
             activeContact: 0,
-            writtenMessage: ""
+            writtenMessage: "",
+            lastMessage : 0,
+            copyOfContacts : 
+            [
+                {
+                    image: "./assets/img/avatar_1.jpg",
+                    fullname: "Giovanni cattaneo",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }]
+                },
+                {
+                    image: "./assets/img/avatar_1.jpg",
+                    fullname: "Fabio De Luigi",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ti sei ricordato di andare a ritirare quella raccomandata in posta?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'E se riesci pass anche al supermercato che abbiamo finito lo zucchero a velo per la torta della nonna',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }]
+                },
+                {
+                    image: "./assets/img/avatar_2.jpg",
+                    fullname: "Michele Lamperti",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai porato il cane dal veterinario per quella pucle??',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }]
+                },
+                {
+                    image: "./assets/img/avatar_3.jpg",
+                    fullname: "Samuele Bonomi",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'riesci ad andare a prendere Carlotta a d equitazione?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+
+                    ]
+                },
+                {
+                    image: "./assets/img/avatar_4.jpg",
+                    fullname: "Alessandro de Bortoli",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si trova in via tagliamento vicno al sentiero che costeggia il lambro',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: "Parlo di quell'orrore della tua macchina che mi ha lasciata a piedi, vai e sistema",
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
+                },
+                {
+                    image: "./assets/img/avatar_5.jpg",
+                    fullname: "Giuliano Vincenti",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
+                },
+                {
+                    image: "./assets/img/avatar_6.jpg",
+                    fullname: "Loretta Scott king",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
+
+                },
+                {
+                    image: "./assets/img/avatar_7.jpg",
+                    fullname: "Luciano Cadorna",
+                    activity: "Non attivo da circa 3h",
+                    hour: "15:45",
+                    messages: [
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di pagarlo stavolta non come quel cretino di tuo padre che a momenti se lo scordava',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
+                },
+            ],
+            
 
         }
 
@@ -198,8 +382,17 @@ createApp({
 
     methods: {
 
+        // $(message(index) {
+        //     lastMessage = lastMessage : this.contacts[this.activeContact].messages.message
+        // }),
+
+        
+
         contactClick(index) {
             console.log("Hello world");
+
+            
+            
 
             if (index >= 0 && index < this.contacts.length) {
                 this.activeContact = index
@@ -207,28 +400,22 @@ createApp({
 
         },
 
+
         contactSearch() {
             console.log("hello word")
 
             let textSearch = this.inputText.toLowerCase().trim()// prende il testo dell'input, lo mette in minuscolo e rimuove eventuali spazi all'inizio e alla fine
-
-            this.inputText = ""
-
-            // if (textSearch === "") {
-            //return, capire come riportare la lista di contatti alla situazione di partenza
-            // }
             this.contacts = this.contacts.filter(function (contact) {
 
-                if (contact.fullname.toLowerCase().includes(textSearch)) { // filtra e restiuisce il fullname in minuscolo se corrisponde a quello dell'input
-                    
+                 if (contact.fullname.toLowerCase().includes(textSearch)) { // filtra e restiuisce il fullname in minuscolo se corrisponde a quello dell'input
                     return contact.fullname
                 } 
             })
+        },
 
-            if (this.inputText === "") {
-                console.log("it doesent work");
-            }
-
+        contactRestore(){
+            this.inputText = ""
+            this.contacts = this.copyOfContacts
         },
 
         writeMessage() {
